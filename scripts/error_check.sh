d9f6463c7533a3ed4e6497f22f3d6e77f0721482
Andrei Chiper <80176224+andreiaky@users.noreply.github.com>
2023-07-06 12:11:17 +0300

    Add files via upload

---
+++ scripts/error_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Begin message
+echo "Input log file to read (insert full path name): "
+read USERINPUT
+
+# We store in logfile the name of the file with basename command
+LOGFILE=$(basename $USERINPUT)
+# echo $LOGFILE
+# echo
+
+# If it finds an error in the file it outputs the first echo else no errors
+if [[ `cat $USERINPUT | grep -Eioe error` = "error" ]]
+then
+	echo "Errors found in log $LOGFILE"
+else
+	echo "No errors found"
+fi
